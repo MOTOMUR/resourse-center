0654af3bd291bc0ea258e9b7cb7508c0c425bc36
Murakami Motoya <owner@MurakaminoMacBook-Pro.local>
2017-10-02 11:06:52 +0900

    it is first Appber with side menu bar

--- src/App.js
+++ src/App.js
@@ -2,16 +2,39 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import AppBar from 'material-ui/AppBar';
+import Drawer from 'material-ui/Drawer';
+import MenuItem from 'material-ui/MenuItem';
+import injectTapEventPlugin from 'react-tap-event-plugin';
+import FontIcon from 'material-ui/FontIcon';
+
+injectTapEventPlugin()
 
 
 class App extends Component {
+
+  constructor(props){
+    super(props);
+    this.state={open: false};
+  }
+
+
   render() {
     return (
       <div className="App">
         <AppBar
           title="plz donation"
           iconClassNameRight="muidocs-icon-navigation-expand-more"
+          onLeftIconButtonTouchTap={() => this.setState({open: !this.state.open})}
         />
+        <Drawer
+          open={this.state.open}
+          docked={false}
+          onRequestChange={(open)=>this.setState({open})}
+        >
+          <MenuItem>Menu Item</MenuItem>
+          <MenuItem>Menu Item 2</MenuItem>
+        </Drawer>
+
       </div>
     );
   }
