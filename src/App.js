87acd40dafb0e1cfdf1ff6b527be92aa4c221713
Murakami Motoya <owner@MurakaminoMacBook-Pro.local>
2017-10-02 14:10:06 +0900

    link make to Drower

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {BrowserRouter,Route,Link} from 'react-router-dom'
 import AppBar from 'material-ui/AppBar';
 import Drawer from 'material-ui/Drawer';
 import MenuItem from 'material-ui/MenuItem';
+import {MuiThemeProvider} from 'material-ui/styles';
 import injectTapEventPlugin from 'react-tap-event-plugin';
 import FontIcon from 'material-ui/FontIcon';
 
@@ -17,25 +19,43 @@ class App extends Component {
     this.state={open: false};
   }
 
+  handleClose = () =>this.setState({open: false})
 
   render() {
     return (
-      <div className="App">
-        <AppBar
-          title="plz donation"
-          iconClassNameRight="muidocs-icon-navigation-expand-more"
-          onLeftIconButtonTouchTap={() => this.setState({open: !this.state.open})}
-        />
-        <Drawer
-          open={this.state.open}
-          docked={false}
-          onRequestChange={(open)=>this.setState({open})}
-        >
-          <MenuItem>Menu Item</MenuItem>
-          <MenuItem>Menu Item 2</MenuItem>
-        </Drawer>
+      <BrowserRouter>
+        <MuiThemeProvider>
+          <div>
+            <AppBar
+              title="plz donation"
+              iconClassNameRight="muidocs-icon-navigation-expand-more"
+              onLeftIconButtonTouchTap={() =>
+                this.setState({open: !this.state.open})}
+            />
+            <Drawer
+              open={this.state.open}
+              docked={false}
+              onRequestChange={open => this.setState({ open })}
+            >
 
-      </div>
+              <Link to='/'><MenuItem onTouchTap={this.handleClose}>
+                Home
+              </MenuItem></Link>
+
+              <Link to='/regist'><MenuItem onTouchTap={this.handleClose}>
+                registeration
+              </MenuItem></Link>
+
+            </Drawer>
+
+            <Route exact path='/' render={() => <h1>Home View</h1>} />
+            <Route
+              exact
+              path='/regist'
+              render={() => <h1>registeration</h1>} />
+          </div>
+        </MuiThemeProvider>
+      </BrowserRouter>
     );
   }
 }
--- src/index.js
+++ src/index.js
@@ -1,9 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {MuiThemeProvider} from 'material-ui/styles';
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<MuiThemeProvider><App /></MuiThemeProvider>, document.getElementById('root'));
+
+const muiTheme = getMuiTheme({
+  palette: {
+    primaryColor: '#21412a',
+  },
+  appBar: {
+    height: 50,
+  },
+});
+
+
+ReactDOM.render(<MuiThemeProvider muiTheme={muiTheme}><App /></MuiThemeProvider>, document.getElementById('root'));
 registerServiceWorker();
